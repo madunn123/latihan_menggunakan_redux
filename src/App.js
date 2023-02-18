49ff3153a49ad259af6a4a584ac35e2b195ea4f2
madunn123 <82297817+madunn123@users.noreply.github.com>
2023-02-18 23:08:50 +0700

    2 files modified, 12 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import GoalsList from './components/GoalsList';
+import TodoList from './components/TodosList';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <TodoList />
+      <GoalsList />
+    </>
   );
 }
+
+export default App;
